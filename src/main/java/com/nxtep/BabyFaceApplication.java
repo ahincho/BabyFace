fb06de1987fc2703998ad55c2908b06292898f44
ahincho <ahincho@unsa.edu.pe>
2025-03-21 05:53:49 -0500

    America Lima Time Zone

--- src/main/java/com/nxtep/BabyFaceApplication.java
+++ src/main/java/com/nxtep/BabyFaceApplication.java
@@ -1,11 +1,19 @@
 package com.nxtep;
 
+import jakarta.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 public class BabyFaceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BabyFaceApplication.class, args);
+	}
+	@PostConstruct
+	public void initialize() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Lima"));
 	}
 }
